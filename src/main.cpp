587b4a5a8c6749dd05373a3ac07d48a91206ff55
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-05-30 00:55:06 +0800

    完成初步框架

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,245 @@
 #include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
 
 using std::cout;
+using std::make_pair;
+using std::map;
+using std::pair;
+using std::priority_queue;
+using std::set;
+using std::string;
+using std::vector;
+
+int rand(int n) {
+    return rand() % n;
+}
+
+class Simulation;
+
+class Event {
+public:
+    const double time;
+
+    Event(double t, Simulation& sim): time(t), sim(sim) {}
+    virtual void process_event() = 0;
+
+protected:
+    Simulation& sim;
+};
+
+struct EventComparator {
+    bool operator()(Event* e1, Event* e2) {
+        return e1->time > e2->time;
+    }
+};
+
+struct Route {
+    string src;
+    string dst;
+    double time;
+    double cost;
+};
+
+struct Station {
+    string id;
+    double time_on_belt;
+    double time_process;
+    set<string> unprocessed_packages;
+};
+
+enum class PackageCategory {
+    STANDARD,
+    EXPRESS,
+};
+
+struct Package {
+    string id;
+    PackageCategory category;
+    double time_created;
+    string src;
+    string dst;
+};
+
+class Simulation {
+private:
+    std::priority_queue<Event*, std::vector<Event*, std::allocator<Event*>>, EventComparator>
+        event_queue;
+    int arrived = 0;
+    double total_time = 0;
+
+    int id = 0;
+
+public:
+    map<string, Station> stations;
+    map<string, vector<Route>> routes;
+    map<string, Package> packages;
+
+public:
+    double time;
+
+    void run();
+    void schedule_event(Event* event) {
+        this->event_queue.push(event);
+    }
+    void add_order(double time, PackageCategory ctg, string src, string dst) {
+        this->id += 1;
+        string id = std::to_string(this->id);
+        this->packages[id] = Package { id, ctg, time, src, dst };
+    }
+    // add station
+    void add_station(string id, double time_on_belt, double time_process) {
+        Station station;
+        station.id = id;
+        station.time_on_belt = time_on_belt;
+        station.time_process = time_process;
+        this->stations[id] = station;
+    }
+    // add route
+    void add_route(string src, string dst, double time, double cost) {
+        auto route = Route { src, dst, time, cost };
+        this->routes[src].push_back(route);
+    }
+    // arrive package
+    void arrive(string package, double time) {
+        this->total_time += time - this->packages[package].time_created;
+        this->arrived += 1;
+    }
+};
+
+void Simulation::run() {
+    while (!this->event_queue.empty()) {
+        Event* event = this->event_queue.top();
+        this->event_queue.pop();
+        this->time = event->time;
+        event->process_event();
+        cout << "arrived: " << this->arrived << ", "; // "arrived: 1\n"
+        cout << "time cost: " << this->total_time << "\n";
+        delete event;
+    }
+}
+
+class PackageArrival: public Event {
+public:
+    PackageArrival(double t, Simulation& sim, string package, string dst):
+        Event(t, sim),
+        package(package),
+        dst(dst) {}
+
+    void process_event() override;
+
+private:
+    string package;
+    string dst;
+};
+
+class PackageStartBelt: public Event {
+public:
+    PackageStartBelt(double t, Simulation& sim, string package, string src, string dst):
+        Event(t, sim),
+        package(package),
+        src(src),
+        dst(dst) {}
+
+    void process_event() override;
+
+private:
+    string package;
+    string src;
+    string dst;
+};
+
+class PackageStartProcessing: public Event {
+public:
+    PackageStartProcessing(double t, Simulation& sim, string package, string src, string dst):
+        Event(t, sim),
+        package(package),
+        src(src),
+        dst(dst) {}
+
+    void process_event() override;
+
+private:
+    string package;
+    string src;
+    string dst;
+};
+
+// [可能是送原地，即完成配送]
+class PackageStartSend: public Event {
+public:
+    PackageStartSend(double t, Simulation& sim, string package, string src, string dst):
+        Event(t, sim),
+        package(package),
+        src(src),
+        dst(dst) {}
+
+    void process_event() override {
+        cout << "[" << this->time << "] PackageStartSend pack" << this->package << ": " << this->src
+             << " => " << this->dst << "\n";
+        if (this->sim.packages[this->package].dst == this->src) {
+            // package has arrived
+            this->sim.arrive(this->package, this->time);
+            return;
+        }
+        this->sim.schedule_event(new PackageArrival(
+            this->time + this->sim.routes[this->src][0].time,
+            this->sim,
+            this->package,
+            this->dst
+        ));
+    }
+
+private:
+    string package;
+    string src;
+    string dst;
+};
+
+void PackageArrival::process_event() {
+    cout << "[" << this->time << "] PackageArrival pack" << this->package << ": " << this->dst
+         << "\n";
+    this->sim.schedule_event(
+        new PackageStartBelt(this->time, this->sim, this->package, this->dst, this->dst)
+    );
+}
+
+void PackageStartBelt::process_event() {
+    cout << "[" << this->time << "] PackageStartBelt pack" << this->package << ": " << this->src
+         << " => " << this->dst << "\n";
+    this->sim.schedule_event(new PackageStartProcessing(
+        this->time + this->sim.stations[this->src].time_on_belt,
+        this->sim,
+        this->package,
+        this->src,
+        this->dst
+    ));
+}
+
+void PackageStartProcessing::process_event() {
+    cout << "[" << this->time << "] PackageStartProcessing pack" << this->package << ": "
+         << this->src << " => " << this->dst << "\n";
+    this->sim.schedule_event(new PackageStartSend(
+        this->time + this->sim.stations[this->src].time_process,
+        this->sim,
+        this->package,
+        this->src,
+        this->dst
+    ));
+}
+
 int main() {
-    cout << "Hello, World!";
+    cout << "Begin\n";
+    Simulation sim;
+    sim.add_station("a", 1.0 / 6, 2);
+    sim.add_station("b", 1.0 / 5, 2);
+    sim.add_route("a", "b", 100, 1200);
+    sim.add_order(100, PackageCategory::STANDARD, "a", "b");
+    sim.schedule_event(new PackageStartBelt(102, sim, "1", "a", "b"));
+    sim.run();
+    cout << "End\n";
     return 0;
 }
---
+++ src/ref.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+// Return a random integer in the range 0 to n-1.
+int irand(int n) {
+    return rand() % n;
+}
+
+class event {
+public:
+    // Construct sets time of event.
+    event(unsigned int t): time(t) {}
+
+    // Execute event by invoking this method.
+    virtual void processEvent() = 0;
+
+    const unsigned int time;
+};
+
+struct eventComparator {
+    bool operator()(const event* left, const event* right) const {
+        return left->time > right->time;
+    }
+};
+
+class simulation {
+public:
+    simulation(): time(0), eventQueue() {}
+    void run();
+    void scheduleEvent(event* newEvent) {
+        eventQueue.push(newEvent);
+    }
+    unsigned int time;
+
+protected:
+    std::priority_queue<event*, std::vector<event*, std::allocator<event*>>, eventComparator>
+        eventQueue;
+};
+
+void simulation::run() {
+    while (!eventQueue.empty()) {
+        event* nextEvent = eventQueue.top();
+        eventQueue.pop();
+        time = nextEvent->time;
+        nextEvent->processEvent();
+        delete nextEvent;
+    }
+}
+
+class storeSimulation: public simulation {
+public:
+    storeSimulation(): simulation(), freeChairs(35), profit(0.0) {}
+    bool canSeat(unsigned int numberOfPeople);
+    void order(unsigned int numberOfScoops);
+    void leave(unsigned int numberOfPeople);
+    // Data fields.
+    unsigned int freeChairs;
+    double profit;
+} theSimulation;
+
+bool storeSimulation::canSeat(unsigned int numberOfPeople) {
+    std::cout << "Time: " << time;
+    std::cout << " group of " << numberOfPeople << " customers arrives";
+
+    if (numberOfPeople < freeChairs) {
+        std::cout << " is seated\n";
+        freeChairs -= numberOfPeople;
+        return true;
+    } else {
+        std::cout << " no room, they leave\n";
+        return false;
+    }
+}
+
+void storeSimulation::order(unsigned int numberOfScoops) {
+    std::cout << "Time: " << time << " serviced order for " << numberOfScoops << '\n';
+    profit += 0.35 * numberOfScoops;
+}
+
+void storeSimulation::leave(unsigned int numberOfPeople) {
+    std::cout << "Time: " << time << " group of size " << numberOfPeople << " leaves\n";
+    freeChairs += numberOfPeople;
+}
+
+class leaveEvent: public event {
+public:
+    leaveEvent(unsigned int t, unsigned int groupSize): event(t), size(groupSize) {}
+    virtual void processEvent();
+
+private:
+    unsigned int size;
+};
+
+void leaveEvent::processEvent() {
+    theSimulation.leave(size);
+}
+
+class orderEvent: public event {
+public:
+    orderEvent(unsigned int t, unsigned int groupSize): event(t), size(groupSize) {}
+    virtual void processEvent();
+
+private:
+    unsigned int size;
+};
+
+void orderEvent::processEvent() {
+    // Each person orders some number of scoops.
+    for (unsigned int i = 0; i < size; i++)
+        theSimulation.order(1 + irand(4));
+
+    // Then we schedule the leave event.
+    theSimulation.scheduleEvent(new leaveEvent(time + 1 + irand(10), size));
+}
+
+class arriveEvent: public event {
+public:
+    arriveEvent(unsigned int t, unsigned int groupSize): event(t), size(groupSize) {}
+    virtual void processEvent();
+
+private:
+    unsigned int size;
+};
+
+void arriveEvent::processEvent() {
+    if (theSimulation.canSeat(size))
+        theSimulation.scheduleEvent(new orderEvent(time + 1 + irand(4), size));
+}
+
+int main() {
+    std::cout << "Ice Cream Store simulation from Chapter 9\n";
+
+    // Load queue with some number of initial events.
+    for (unsigned t = 0; t < 20; t += irand(6)) {
+        std::cout << "pumping queue with event " << t << '\n';
+        theSimulation.scheduleEvent(new arriveEvent(t, 1 + irand(4)));
+    }
+
+    // Run the simulation.
+    theSimulation.run();
+
+    std::cout << "Total profits " << theSimulation.profit
+              << "\nEnd of ice cream store simulation\n";
+
+    return 0;
+}
