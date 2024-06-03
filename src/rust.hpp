c776e2584b4b44044676f83138929d0909401ab1
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-06-04 02:02:01 +0800

    inline time_ok()

--- src/rust.hpp
+++ src/rust.hpp
@@ -4,9 +4,10 @@
 namespace rust {
 constexpr double EPS = 1e-9;
 
-bool time_ok(double time, double ok_time) {
+inline bool time_ok(double time, double ok_time) {
     return time > ok_time - EPS;
 }
+
 } // namespace rust
 
 #endif
